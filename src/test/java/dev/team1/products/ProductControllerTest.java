7a143473c3cec106ad363ad7594d669978d641a2
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-09-11 18:23:18 +0200

    test (GS-496): ProductControllerTest - should return all available second page

--- src/test/java/dev/team1/products/ProductControllerTest.java
+++ src/test/java/dev/team1/products/ProductControllerTest.java
@@ -18,6 +18,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -54,10 +56,12 @@ public class ProductControllerTest {
 
     @Test 
     void testIndex_shouldReturnAllAvailableFirstPage() throws Exception {
+        Pageable pageable = PageRequest.of(0, 20);
+        
         List<ProductDTOResponse> mockContent = mockProducts.stream()
             .filter(p -> p.available())
             .collect(Collectors.toList());
-        Pageable pageable = PageRequest.of(0, 20);
+        
         Page<ProductDTOResponse> mockPage = new PageImpl<>(mockContent, pageable, mockContent.size());
         String json = mapper.writeValueAsString(mockPage);
 
@@ -76,25 +80,51 @@ public class ProductControllerTest {
 
         assertThat(response.getContentAsString(), is(equalTo(json)));
         assertThat(respContent, is(equalTo(mockContent)));
-
+        assertThat(response.getStatus(), is(equalTo(HttpStatus.OK.value())));
+        assertThat(tree.get("totalPages").asInt(), is(equalTo(1)));
+        assertThat(respContent.size(), is(equalTo(7)));
+        assertThat(respContent.get(0).name(), is(equalTo("Ramen Fix")));
     }
 
 
     @Test 
     void testIndex_shouldReturnAllAvailableSecondPage() throws Exception {
+        Pageable pageable = PageRequest.of(1, 4);
+        
+        List<ProductDTOResponse> mockContent = mockProducts.stream()
+            .filter(p -> p.available())
+            .collect(Collectors.toList());
+        
+        int start = (int) pageable.getOffset();
+        int end = Math.min(start + pageable.getPageSize(), mockContent.size());
 
-        // создаем мок ответа объектом
+        List<ProductDTOResponse> mockPageContent = mockContent.subList(start, end);
 
-        // конвертируем в мок ответ json
+        Page<ProductDTOResponse> mockPage = new PageImpl<>(mockPageContent, pageable, mockContent.size());
+        String json = mapper.writeValueAsString(mockPage);
 
-        // подменяем ответ от service нашим mock-объектом
+        when(service.getAllAvailable(pageable)).thenReturn(mockPage);
+        MockHttpServletResponse response = mockMvc.perform(get("/api/v1/products")
+                .param("page", "1")
+                .param("size", "4")
+                .accept(MediaType.ALL_VALUE))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse();
 
-        // Имитируем запрос
+        JsonNode tree = mapper.readTree(response.getContentAsString());
 
-        // конвертируем ответ в объект 
+        List<ProductDTOResponse> respContent = mapper.convertValue(
+            tree.get("content"),
+            new TypeReference<List<ProductDTOResponse>>() {}
+        );
 
-        // сравниваем и проверяем
-
+        assertThat(response.getContentAsString(), is(equalTo(json)));
+        assertThat(respContent, is(equalTo(mockPageContent)));
+        assertThat(response.getStatus(), is(equalTo(HttpStatus.OK.value())));
+        assertThat(tree.get("totalPages").asInt(), is(equalTo(2)));
+        assertThat(respContent.size(), is(equalTo(3)));
+        assertThat(respContent.get(0).name(), is(equalTo("EbiOps")));
     }
 
     @Test 
