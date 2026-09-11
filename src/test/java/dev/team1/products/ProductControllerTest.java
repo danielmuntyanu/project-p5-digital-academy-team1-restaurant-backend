cd4bea785cf8a55f5bb26fa7f41357616717ad0a
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-09-11 17:40:35 +0200

    test (GS-496): ProductControllerTest - should return all available products

--- src/test/java/dev/team1/products/ProductControllerTest.java
+++ src/test/java/dev/team1/products/ProductControllerTest.java
@@ -2,27 +2,38 @@ package dev.team1.products;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-
-import java.math.BigDecimal;
-import java.util.List;
-
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.containsString;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import dev.team1.config.SecurityConfiguration;
 import dev.team1.contracts.IProductService;
-import dev.team1.enums.ProductCategory;
 import dev.team1.products.dtos.ProductDTOResponse;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.JsonNode;
 
 @WebMvcTest(controllers = ProductController.class)
+@Import(SecurityConfiguration.class)
 public class ProductControllerTest {
 
     @Autowired 
@@ -43,18 +54,28 @@ public class ProductControllerTest {
 
     @Test 
     void testIndex_shouldReturnAllAvailableFirstPage() throws Exception {
+        List<ProductDTOResponse> mockContent = mockProducts.stream()
+            .filter(p -> p.available())
+            .collect(Collectors.toList());
+        Pageable pageable = PageRequest.of(0, 20);
+        Page<ProductDTOResponse> mockPage = new PageImpl<>(mockContent, pageable, mockContent.size());
+        String json = mapper.writeValueAsString(mockPage);
 
-        // создаем мок ответа объектом
+        when(service.getAllAvailable(pageable)).thenReturn(mockPage);
+        MockHttpServletResponse response = mockMvc.perform(get("/api/v1/products"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse();
 
-        // конвертируем в мок ответ json
+        JsonNode tree = mapper.readTree(response.getContentAsString());
 
-        // подменяем ответ от service нашим mock-объектом
+        List<ProductDTOResponse> respContent = mapper.convertValue(
+            tree.get("content"),
+            new TypeReference<List<ProductDTOResponse>>() {}
+        );
 
-        // Имитируем запрос
-
-        // конвертируем ответ в объект 
-
-        // сравниваем и проверяем
+        assertThat(response.getContentAsString(), is(equalTo(json)));
+        assertThat(respContent, is(equalTo(mockContent)));
 
     }
 
